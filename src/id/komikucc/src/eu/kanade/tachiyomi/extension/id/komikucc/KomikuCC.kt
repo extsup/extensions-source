7f377cf58d808ce94f9f41f9f68483dd544dde3b
extsup <ciklah7878@gmail.com>
2026-03-12 20:47:33 +0700

    Update KomikuCC.kt

--- src/id/komikucc/src/eu/kanade/tachiyomi/extension/id/komikucc/KomikuCC.kt
+++ src/id/komikucc/src/eu/kanade/tachiyomi/extension/id/komikucc/KomikuCC.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.id.komiku
+package eu.kanade.tachiyomi.extension.id.komikucc
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
