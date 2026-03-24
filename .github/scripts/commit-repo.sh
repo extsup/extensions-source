fd77a88a9455ad92736835ce5dbda3e6a87a61cd
extsup <239924903+extsup@users.noreply.github.com>
2026-03-25 00:45:56 +0700

    Update commit-repo.sh

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
-git config --global user.name "keiyoushi-bot"
+git config --global user.email "239924903+extsup@users.noreply.github.com"
+git config --global user.name "extsup-bot"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update extensions repo"
     git push
 
-    curl https://purge.jsdelivr.net/gh/keiyoushi/extensions@repo/index.min.json
+    # Hapus atau ubah baris ini sesuai repo kamu
+    # curl https://purge.jsdelivr.net/gh/extsup/extensions@repo/index.min.json
 else
     echo "No changes to commit"
 fi
