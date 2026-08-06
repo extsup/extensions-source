5e707a90c3f2bfc0cc5e5c4afc5a8fffe93caf56
extsup <ciklah7878@gmail.com>
2026-08-06 16:57:53 +0700

    Add Komik Cast Beta extension

---
+++ src/id/komikcastbeta/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Komik Cast Beta"
+    versionCode = 1
+    contentWarning = ContentWarning.SAFE
+    libVersion = "1.4"
+
+    source {
+        lang = "id"
+        baseUrl = "https://v3.komikcast.fit"
+    }
+}
