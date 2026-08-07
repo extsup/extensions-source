994129c05f644d406cf7ac176355ef2efd72432b
extsup <ciklah7878@gmail.com>
2026-08-07 16:28:27 +0700

    Fix repo metadata to use extsup instead of Keiyoushi

--- .github/scripts/publish-repo.py
+++ .github/scripts/publish-repo.py
@@ -38,9 +38,9 @@ REPO_APK_DIR.mkdir(parents=True, exist_ok=True)
 REPO_JAR_DIR.mkdir(parents=True, exist_ok=True)
 REPO_ICON_DIR.mkdir(parents=True, exist_ok=True)
 
-APK_BASE_URL = "https://cdn.jsdelivr.net/gh/keiyoushi/extensions@repo/apk"
-JAR_BASE_URL = "https://raw.githubusercontent.com/keiyoushi/extensions/repo/jar"
-ICON_BASE_URL = "https://cdn.jsdelivr.net/gh/keiyoushi/extensions@repo/icon"
+APK_BASE_URL = "https://cdn.jsdelivr.net/gh/extsup/extensions@repo/apk"
+JAR_BASE_URL = "https://raw.githubusercontent.com/extsup/extensions/repo/jar"
+ICON_BASE_URL = "https://cdn.jsdelivr.net/gh/extsup/extensions@repo/icon"
 
 to_delete: list[str] = json.loads(sys.argv[1])
 
@@ -131,11 +131,11 @@ all_extensions.extend(new_extensions)
 all_extensions.sort(key=lambda ext: ext.packageName)
 
 index = index_pb2.Index(
-    name="Keiyoushi",
-    badgeLabel="KEI",
+    name="extsup",
+    badgeLabel="EXT",
     signingKey="9add655a78e96c4ec7a53ef89dccb557cb5d767489fac5e785d671a5a75d4da2",
     contact=index_pb2.Contact(
-        website="https://keiyoushi.github.io", discord="https://discord.gg/3FbCpdKbdY"
+        website="https://github.com/extsup/extensions"
     ),
     extensionList=index_pb2.ExtensionList(extensions=all_extensions),
 )
