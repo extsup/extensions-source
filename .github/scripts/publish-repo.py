513d6efe045797569f8d223ea0e5013be8a81a1f
extsup <ciklah7878@gmail.com>
2026-08-09 04:01:49 +0700

    fix: repo name typo Extsups -> Extsup

--- .github/scripts/publish-repo.py
+++ .github/scripts/publish-repo.py
@@ -195,7 +195,7 @@ with REPO_DIR.joinpath("index.min.json").open("w", encoding="utf-8") as f:
 # Generate repo.json
 repo_json = {
     "meta": {
-        "name": "Extsups",
+        "name": "Extsup",
         "shortName": "extsup",
         "website": "https://github.com/extsup/extensions",
         "signingKeyFingerprint": "af8dce867726424977496de4c45340bcf7388c184b75a823b6d7d69bee8fafd4"
