b25dcb4ac019d64d7fa44ebb1add940123e9e871
extsup <ciklah7878@gmail.com>
2026-08-09 15:35:14 +0700

    fix: restore build.gradle.kts versionCode 5

---
+++ src/id/mikoroku/build.gradle.kts
@@ -0,0 +1,17 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "MikoRoku"
+    versionCode = 5
+    contentWarning = ContentWarning.MIXED
+    libVersion = "1.6"
+
+    source {
+        lang = "id"
+        baseUrl = "https://mikoroku.com"
+    }
+}
