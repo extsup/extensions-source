a935bd11f551a708d221d7d2c334db6ba3a81157
extsup <apkfile007@gmail.com>
2026-08-10 19:58:39 +0700

    fix: replace removeLast() for Android compatibility, restore MGKomik, add madara lib

--- src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
+++ src/id/mgkomik/src/eu/kanade/tachiyomi/extension/id/mgkomik/MGKomik.kt
@@ -24,7 +24,7 @@ abstract class MGKomik : Madara() {
         set("Sec-CH-UA-Model", "\"\"")
     }
 
-    override val client = network.cloudflareClient.newBuilder()
+    override val client = network.client.newBuilder()
         .addInterceptor { chain ->
             val request = chain.request()
             val path = request.url.encodedPath
