8b1ccbd29fb8c692e786413e119d93496c4f68ff
extsup <ciklah7878@gmail.com>
2026-08-31 00:33:24 +0700

    Add deeplink support for /series/ URLs

--- src/id/shinigamibeta/src/eu/kanade/tachiyomi/extension/id/shinigamibeta/ShinigamiBeta.kt
+++ src/id/shinigamibeta/src/eu/kanade/tachiyomi/extension/id/shinigamibeta/ShinigamiBeta.kt
@@ -22,6 +22,8 @@ import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import rx.Observable
 import okhttp3.Request
 import okhttp3.Response
 
